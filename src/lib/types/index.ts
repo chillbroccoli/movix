4390d0add4dff666f8f516612ff4e8d94a340d54
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-23 13:44:47 +0100

    add TvShow and Resource types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,4 +1,4 @@
-export type TrendingResource = {
+export type Trending = {
   id: number;
   adult: boolean;
   backdrop_path?: string;
@@ -33,4 +33,41 @@ export type Movie = {
   vote_count: number;
   video?: boolean;
   vote_average: number;
+};
+
+export type TvShow = {
+  id: number;
+  poster_path?: string;
+  popularity: number;
+  backdrop_path?: string;
+  vote_average: number;
+  overview: string;
+  first_air_date: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
+  name: string;
+  original_name: string;
+};
+
+export type Resource = {
+  id: number;
+  poster_path?: string;
+  popularity?: number;
+  backdrop_path?: string;
+  vote_average?: number;
+  vote_count?: number;
+  overview?: string;
+  first_air_date?: string;
+  release_date?: string;
+  genre_ids?: number[];
+  name?: string;
+  original_name?: string;
+  original_title?: string;
+  original_language?: string;
+  title?: string;
+  media_type?: "movie" | "tv";
+  video?: boolean;
+  origin_country?: string[];
 };
