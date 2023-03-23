e28a9c31935678cbdd438b15b618f3f2eae9d43c
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-23 13:44:29 +0100

    add generic type for getRandomResource

--- src/lib/helpers/getRandomResource.ts
+++ src/lib/helpers/getRandomResource.ts
@@ -1,7 +1,5 @@
-import { TrendingResource } from "../types";
-
-export const getRandomResource = (list: TrendingResource[]) => {
+export function getRandomResource<T>(list: T[]) {
   const shuffled = list.sort(() => 0.5 - Math.random());
   const randomIndex = Math.floor(Math.random() * shuffled.length);
   return shuffled[randomIndex];
-};
+}
