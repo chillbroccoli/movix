9b5587b3b6bb3c3f9449eba8a3a41271d4e331d8
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-23 13:42:06 +0100

    add type for random resource

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import { Carousel } from "~/components/Carousel";
 import { Featured } from "~/components/Featured";
 import { getTrending } from "~/lib/api";
 import { getRandomResource } from "~/lib/helpers/getRandomResource";
+import { Movie, TvShow } from "~/lib/types";
 
 export default async function HomePage() {
   const moviesData = await getTrending("movie");
   const tvData = await getTrending("tv");
 
-  const randomTrending = getRandomResource([...moviesData.results, ...tvData.results]);
+  const randomTrending = getRandomResource<Movie | TvShow>([
+    ...moviesData.results,
+    ...tvData.results,
+  ]);
 
   return (
     <div className="w-full">
