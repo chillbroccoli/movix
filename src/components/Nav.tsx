c012f679cbd194b2fb3ecabf59be4cb6be66e90e
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-24 13:46:24 +0100

    adjust to display only icons, add icon button for genres

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,28 @@
+import { IconMenu2 } from "@tabler/icons-react";
 import Link from "next/link";
 import { SIDE_NAV } from "~/lib/constants";
 
 export function Nav() {
   return (
     <div className="mt-10">
-      <ul className="space-y-4">
+      <ul className="space-y-6">
         {SIDE_NAV.map((item) => (
           <li
             key={item.href}
             className="transition-colors duration-200 ease-in-out rounded-md hover:bg-pink-500/30"
           >
-            <Link
-              href={item.href}
-              className="flex w-full h-full gap-4 p-3 pl-0"
-            >
-              <div className="pl-2">
+            <Link href={item.href} className="flex w-full h-full gap-4 p-2">
+              <div>
                 <item.icon size={24} />
               </div>
-              <span className="text-xl">{item.name}</span>
             </Link>
           </li>
         ))}
+        <button className="transition-colors duration-200 ease-in-out rounded-md hover:bg-pink-500/30">
+          <div className="flex w-full h-full gap-4 p-2">
+            <IconMenu2 size={24} />
+          </div>
+        </button>
       </ul>
     </div>
   );
