db09973f254374b93e63ac1720e90bd42a4c7c16
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-24 13:41:52 +0100

    add head and update widths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="w-full h-full">
+      <head />
       <body className={clsx(inter.className, "h-full bg-zinc-900 text-white")}>
         <div className="w-full h-full">
           <Sidebar />
-          <div className="w-[85%] ml-[15%] h-full">
+          <div className="w-[calc(100%_-_100px)] ml-[100px] h-full">
             <main className="w-full h-full">{children}</main>
           </div>
         </div>
