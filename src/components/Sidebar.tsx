ae8b08942b061f6df7a546104fabb05b005a0def
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-24 13:46:33 +0100

    make sidebar 100px width

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,14 +2,14 @@
 
 import { Logo } from "./Logo";
 import { Nav } from "./Nav";
-import { SearchBar } from "./SearchBar";
 
 export function Sidebar() {
   return (
-    <div className="fixed top-0 bottom-0 left-0 w-[15%] h-full p-6 px-8 border-r border-pink-500/30">
-      <Logo />
-      <Nav />
-      <SearchBar />
+    <div className="fixed top-0 bottom-0 left-0 w-[100px] h-full border-r border-pink-500/30">
+      <div className="flex flex-col items-center justify-center pt-6">
+        <Logo />
+        <Nav />
+      </div>
     </div>
   );
 }
