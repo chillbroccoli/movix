93c5fbf7e02b9234fd7c185a2dc613bd70fa0d18
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:27:51 +0100

    add TvShowDetails type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -97,3 +97,61 @@ export type MovieDetails = {
   revenue: number;
   runtime: number;
 };
+
+export type TvShowDetails = {
+  id: number;
+  backdrop_path?: string;
+  poster_path?: string;
+  created_by: {
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path?: string;
+  }[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: { id: number; name: string }[];
+  homepage?: string;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path?: string;
+    vote_average: number;
+    vote_count: number;
+  };
+  name: string;
+  networks: {
+    id: number;
+    logo_path?: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_companies: { id: number; name: string; logo_path?: string; origin_country: string }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path?: string;
+    season_number: number;
+  }[];
+  spoken_languages: { iso_639_1: string; name: string; english_name: string }[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+  number_of_episodes: number;
+  number_of_seasons: number;
+};
