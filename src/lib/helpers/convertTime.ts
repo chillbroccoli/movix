e583946a4352995c3e9d69013be160b7fe38cbcf
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:27:30 +0100

    hnadle minutes only

--- src/lib/helpers/convertTime.ts
+++ src/lib/helpers/convertTime.ts
@@ -1,5 +1,10 @@
 export function convertTime(num: number) {
   const hours = Math.floor(num / 60);
   const minutes = num % 60;
+
+  if (hours === 0) {
+    return `${minutes}m`;
+  }
+
   return `${hours}h ${minutes}m`;
 }
