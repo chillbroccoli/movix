5914e58b7c6eeb9902929f1df698a297167d37b0
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:27:21 +0100

    add multiSearch api and handle errors

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,40 +3,103 @@ const API_KEY = process.env.API_KEY;
 
 export async function getDetails(type: "movie" | "tv", id: string) {
   const res = await fetch(`${BASE_URL}/${type}/${id}?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getTrending(type: "movie" | "tv") {
   const res = await fetch(`${BASE_URL}/trending/${type}/day?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getTopRated(type: "movie" | "tv") {
   const res = await fetch(`${BASE_URL}/${type}/top_rated?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getPopular(type: "movie" | "tv") {
   const res = await fetch(`${BASE_URL}/${type}/popular?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getMoviesNowPlaying() {
   const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getLatest(type: "movie" | "tv") {
   const res = await fetch(`${BASE_URL}/${type}/latest?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getAiringToday() {
   const res = await fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
 
 export async function getSimilar(type: "movie" | "tv", id: string) {
   const res = await fetch(`${BASE_URL}/${type}/${id}/similar?api_key=${API_KEY}`);
-  return res.json();
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
+}
+
+export async function multiSearch(query?: string) {
+  if (!query) {
+    return null;
+  }
+
+  const res = await fetch(`${BASE_URL}/search/multi?api_key=${API_KEY}&query=${query}`);
+  const data = await res.json();
+
+  if (data.success === false) {
+    throw new Error(data.status_message);
+  }
+
+  return data;
 }
