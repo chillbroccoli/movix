5c04c58dd256d678ecdcced2533d03340fb90915
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:15:23 +0100

    add Link to the Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "~/images/movix-color.png";
 
 export function Logo() {
   return (
     <div className="relative w-16 h-16">
-      <Image src={logo} alt="Logo" fill />
+      <Link href="/">
+        <Image src={logo} alt="Logo" fill />
+      </Link>
     </div>
   );
 }
