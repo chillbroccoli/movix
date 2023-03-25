7229342b7e78088c12ef046a2af05b77c2ef5ac8
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:13:58 +0100

    add image fallback

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,10 +2,10 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Tilt from "react-parallax-tilt";
 
 import { POSTER_IMAGE } from "~/lib/constants";
 import { Resource } from "~/lib/types";
+import imageNotFound from "~/images/image_not_available.png";
 
 export function Carousel({
   items,
@@ -17,8 +17,8 @@ export function Carousel({
   hrefType?: "movies" | "tv";
 }) {
   return (
-    <div className="p-4 mt-2">
-      <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+    <div>
+      <h2 className="text-2xl font-bold">{title}</h2>
 
       <div className="flex p-4 space-x-6 overflow-x-scroll flex-nowrap snap-x">
         {items.map((item) => (
@@ -31,20 +31,18 @@ export function Carousel({
 
 function CarouselItem({ item, hrefType }: { item: Resource; hrefType: "movies" | "tv" }) {
   return (
-    <Tilt>
-      <Link href={`/${hrefType}/${item.id}`} className="relative rounded-md group">
-        <div className="snap-start w-[225px] h-[350px] flex-shrink-0 shadow-md shadow-white/20 rounded-md overflow-hidden">
-          <div className="relative w-full h-full">
-            <Image
-              src={`${POSTER_IMAGE.W342}${item.poster_path}`}
-              alt={item?.title ?? item?.name ?? ""}
-              fill
-            />
-          </div>
+    <Link href={`/${hrefType}/${item.id}`} className="relative rounded-md group">
+      <div className="snap-start w-[225px] h-[350px] flex-shrink-0 shadow-md shadow-white/20 rounded-md overflow-hidden">
+        <div className="relative w-full h-full">
+          <Image
+            src={item.poster_path ? `${POSTER_IMAGE.W342}${item.poster_path}` : imageNotFound}
+            alt={item?.title ?? item?.name ?? ""}
+            fill
+          />
         </div>
+      </div>
 
-        <div className="absolute top-0 bottom-0 left-0 right-0 transition-all duration-200 ease-in-out bg-black/20 group-hover:bg-black/50"></div>
-      </Link>
-    </Tilt>
+      <div className="absolute top-0 bottom-0 left-0 right-0 transition-all duration-200 ease-in-out bg-black/20 group-hover:bg-black/50"></div>
+    </Link>
   );
 }
