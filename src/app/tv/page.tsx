84c481086c6b6e18e05fc9a13cba9b696f288735
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:13:18 +0100

    sort results for highest votes

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -1,24 +1,34 @@
 import { Featured } from "~/components/Featured";
 import { ItemsList } from "~/components/ItemsList";
-import { getAiringToday, getPopular, getTopRated } from "~/lib/api";
+import { getPopular, getTopRated, getTrending } from "~/lib/api";
 import { getRandomResource } from "~/lib/helpers/getRandomResource";
 import { TvShow } from "~/lib/types";
 
 export default async function TvShowsPage() {
-  const [topRated, popular, airingToday] = await Promise.all([
+  const [topRated, popular, trending] = await Promise.all([
     getTopRated("tv"),
     getPopular("tv"),
-    getAiringToday(),
+    getTrending("tv"),
   ]);
 
   const randomTopRated = getRandomResource<TvShow>(topRated.results);
 
+  const popularSorted = popular.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+  const topRatedSorted = topRated.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+  const trendingSorted = trending.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+
   return (
     <div className="w-full">
       <Featured item={randomTopRated} />
-      <ItemsList items={popular.results} title="Popular" shortened hrefType="tv" />
-      <ItemsList items={topRated.results} title="Top Rated" shortened hrefType="tv" />
-      <ItemsList items={airingToday.results} title="Airing Today" shortened hrefType="tv" />
+      <ItemsList items={topRatedSorted} title="Top Rated" shortened hrefType="tv" />
+      <ItemsList items={popularSorted} title="Popular" shortened hrefType="tv" />
+      <ItemsList items={trendingSorted} title="Trending" shortened hrefType="tv" />
     </div>
   );
 }
