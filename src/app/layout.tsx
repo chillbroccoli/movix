1c5ea36c4baadf839d9ae93eb3a952ca527299d3
Kacper Malek <kacper.malek0828@gmail.com>
2023-03-25 11:12:08 +0100

    add Footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
 import { Sidebar } from "~/components/Sidebar";
+import { Footer } from "~/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={clsx(inter.className, "h-full bg-zinc-900 text-white")}>
         <div className="w-full h-full">
           <Sidebar />
-          <div className="w-[calc(100%_-_100px)] ml-[100px] h-full">
-            <main className="w-full h-full">{children}</main>
+          <div className="w-[calc(100%_-_100px)] ml-[100px] h-full flex flex-col justify-between relative">
+            <main className="w-full">{children}</main>
+            <Footer />
           </div>
         </div>
       </body>
