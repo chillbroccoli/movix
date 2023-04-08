9f2ede789c848365e357c74b82f93b6380e83ab2
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-08 18:04:09 +0200

    update class name

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { IconLoader } from "@tabler/icons-react";
 
 export function Spinner() {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
       <IconLoader className="w-12 h-12 text-pink-500 animate-spin" />
     </div>
   );
