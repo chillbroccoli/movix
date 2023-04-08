b796d48f5db881eccce191a94f51da2b53a98607
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-08 18:02:43 +0200

    add GenresDropdown

---
+++ src/components/GenresDropdown.tsx
@@ -0,0 +1,92 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import * as Tabs from "@radix-ui/react-tabs";
+import { IconMenu2 } from "@tabler/icons-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getGenres } from "~/lib/api";
+
+type Genre = {
+  id: number;
+  name: string;
+};
+
+export function GenresDropdown() {
+  const [moviesGenres, setMoviesGenres] = useState<Genre[]>([]);
+  const [tvGenres, setTvGenres] = useState<Genre[]>([]);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const [moviesGenres, tvGenres] = await Promise.all([getGenres("movie"), getGenres("tv")]);
+
+      setMoviesGenres(moviesGenres.genres);
+      setTvGenres(tvGenres.genres);
+    };
+
+    fetchGenres();
+  }, []);
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild className="outline-none">
+        <button className="transition-colors duration-200 ease-in-out rounded-md hover:bg-pink-500/30">
+          <div className="flex w-full h-full gap-4 p-2">
+            <IconMenu2 size={24} />
+          </div>
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          side="right"
+          className="max-w-[300px] bg-zinc-800 rounded-md shadow-sm shadow-white/20 border border-pink-500/70"
+          sideOffset={5}
+        >
+          <Tabs.Root defaultValue="movies">
+            <Tabs.List className="flex pb-2">
+              <Tabs.Trigger
+                value="movies"
+                className="w-full p-1 data-[state=active]:bg-pink-500/50 border-b border-b-zinc-500 data-[state=active]:border-b-pink-400"
+              >
+                Movies
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                value="tv"
+                className="w-full p-1 data-[state=active]:bg-pink-500/50 border-b border-b-zinc-500 data-[state=active]:border-b-pink-400"
+              >
+                Tv Shows
+              </Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="movies">
+              <div className="flex flex-wrap items-center justify-start gap-2 px-2 pt-2 pb-2">
+                {moviesGenres.map((genre) => (
+                  <DropdownMenu.Item
+                    key={genre.id}
+                    className="p-1 border border-pink-500 rounded-md outline-none data-[highlighted]:bg-pink-500/30 transition-all duration-200 ease-in-out"
+                  >
+                    <Link href={`/movies/genre/${genre.id}/1`}>
+                      <span>{genre.name}</span>
+                    </Link>
+                  </DropdownMenu.Item>
+                ))}
+              </div>
+            </Tabs.Content>
+            <Tabs.Content value="tv">
+              <div className="flex flex-wrap items-center justify-start gap-2 px-2 pt-2 pb-2">
+                {tvGenres.map((genre) => (
+                  <DropdownMenu.Item
+                    key={genre.id}
+                    className="p-1 border border-pink-500 rounded-md outline-none data-[highlighted]:bg-pink-500/30 transition-all duration-200 ease-in-out"
+                  >
+                    <Link href={`/tv/genre/${genre.id}/1`}>
+                      <span>{genre.name}</span>
+                    </Link>
+                  </DropdownMenu.Item>
+                ))}
+              </div>
+            </Tabs.Content>
+          </Tabs.Root>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
