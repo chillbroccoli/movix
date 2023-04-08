47fa3c2675e330c8135c36281c254f4a247dd93c
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-08 18:03:56 +0200

    update class name

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,7 +23,7 @@ export function SearchBar() {
           type="text"
           name="query"
           id="query"
-          className="block w-full px-6 py-4 text-lg text-gray-500 border-0 border-b-2 shadow-sm outline-none bg-zinc-800 border-b-zinc-600 placeholder:text-gray-500 placeholder:text-lg focus:ring-0 focus:border-b-pink-500"
+          className="block w-full px-6 py-4 text-lg text-gray-400 border-0 border-b-2 shadow-sm outline-none bg-zinc-800 border-b-zinc-600 placeholder:text-gray-400 placeholder:text-lg focus:ring-0 focus:border-b-pink-500"
           placeholder="Search..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
