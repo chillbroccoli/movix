8aad149ba5f4dbbea722d5cdf27e1e12eb3976e2
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-10 21:33:56 +0200

    adjust for mobile view

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,9 +5,11 @@ import { Nav } from "./Nav";
 
 export function Sidebar() {
   return (
-    <div className="fixed top-0 bottom-0 left-0 w-[100px] h-full border-r border-pink-500/30">
-      <div className="flex flex-col items-center justify-center pt-6">
-        <Logo />
+    <div className="fixed z-10 lg:z-0 lg:top-0 bottom-0 left-0 right-0 lg:w-[100px] h-[70px] lg:h-full lg:border-r lg:border-pink-500/30 bg-zinc-800 lg:bg-transparent">
+      <div className="flex flex-row items-center justify-center h-full lg:pt-6 lg:flex-col">
+        <div className="hidden lg:block">
+          <Logo />
+        </div>
         <Nav />
       </div>
     </div>
