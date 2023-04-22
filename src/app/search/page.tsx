a99e0a80dd53afa09d1aca0c3ec210acd2ddc449
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-22 23:56:22 +0200

    add SearchParams type, add ViewToggle

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,17 +1,18 @@
 import { ItemsList } from "~/components/ItemsList";
 import { SearchBar } from "~/components/SearchBar";
+import { ViewToggle } from "~/components/ViewToggle";
 import { multiSearch } from "~/lib/api";
+import { SearchParams } from "~/lib/types";
 
-export default async function SearchPage({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+export default async function SearchPage({ searchParams }: { searchParams?: SearchParams }) {
   const data = await multiSearch(searchParams?.q as string);
 
   return (
     <div>
       <SearchBar />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
       {!!data?.results?.length && (
         <ItemsList items={data.results} title={`Search result for: ${searchParams?.q}`} />
       )}
