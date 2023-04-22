b2aad67bf7bce93321952d157b573b911e9b2e1f
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-22 23:55:52 +0200

    remove genre/page page, pass genre and page as url params

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -1,17 +1,33 @@
 import { Featured } from "~/components/Featured";
 import { ItemsList } from "~/components/ItemsList";
-import { getPopular, getTopRated, getTrending } from "~/lib/api";
+import { ViewToggle } from "~/components/ViewToggle";
+import { getByGenre, getGenres, getPopular, getTopRated, getTrending } from "~/lib/api";
 import { getRandomResource } from "~/lib/helpers/getRandomResource";
-import { TvShow } from "~/lib/types";
+import { SearchParams, TvShow } from "~/lib/types";
 
-export default async function TvShowsPage() {
+export default async function TvShowsPage({ searchParams }: { searchParams: SearchParams }) {
   const [topRated, popular, trending] = await Promise.all([
     getTopRated("tv"),
     getPopular("tv"),
     getTrending("tv"),
   ]);
 
-  const randomTopRated = getRandomResource<TvShow>(topRated.results);
+  const values =
+    searchParams?.genre && searchParams?.page
+      ? await Promise.all([
+          getByGenre("tv", searchParams?.genre as string, Number(searchParams.page) ?? 1),
+          getGenres("tv"),
+        ])
+      : null;
+
+  const genre =
+    values &&
+    values[1] &&
+    values[1].genres.find(
+      (genre: { id: number; name: string }) => genre.id === Number(searchParams.genre)
+    );
+
+  const randomResource = getRandomResource<TvShow>(values ? values[0]?.results : topRated.results);
 
   const popularSorted = popular.results.sort(
     (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
@@ -25,10 +41,25 @@ export default async function TvShowsPage() {
 
   return (
     <div className="w-full">
-      <Featured item={randomTopRated} />
-      <ItemsList items={topRatedSorted} title="Top Rated" shortened hrefType="tv" />
-      <ItemsList items={popularSorted} title="Popular" shortened hrefType="tv" />
-      <ItemsList items={trendingSorted} title="Trending" shortened hrefType="tv" />
+      <Featured item={randomResource} />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      {searchParams.genre && searchParams.page && values ? (
+        <ItemsList
+          items={values[0].results}
+          title={genre.name}
+          withPagination
+          currentPage={values[0].page}
+          totalPages={values[0].total_pages}
+        />
+      ) : (
+        <>
+          <ItemsList items={topRatedSorted} title="Top Rated" shortened hrefType="tv" />
+          <ItemsList items={popularSorted} title="Popular" shortened hrefType="tv" />
+          <ItemsList items={trendingSorted} title="Trending" shortened hrefType="tv" />
+        </>
+      )}
     </div>
   );
 }
