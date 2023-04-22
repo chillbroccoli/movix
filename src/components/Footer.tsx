ac8bed6c6b66cd6f3486d47a1b6c094c5961e206
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-22 23:58:42 +0200

    adjust padding bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="flex flex-col items-center justify-center lg:pb-12 font-light text-gray-500 font-sm pb-[80px]">
+    <div className="flex flex-col items-center justify-center lg:pb-12 font-light text-gray-500 font-sm pb-[100px]">
       <p>&copy; {new Date().getFullYear()}. All Rights Reserved.</p>
       <p>
         Data provided by{" "}
