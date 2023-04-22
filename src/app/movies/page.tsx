e52cfbc70e96df9d189169b77fc2c6da21db51ec
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-22 23:55:46 +0200

    remove genre/page page, pass genre and page as url params

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,24 +1,55 @@
 import { Featured } from "~/components/Featured";
 import { ItemsList } from "~/components/ItemsList";
-import { getMoviesNowPlaying, getPopular, getTopRated } from "~/lib/api";
+import { ViewToggle } from "~/components/ViewToggle";
+import { getByGenre, getGenres, getMoviesNowPlaying, getPopular, getTopRated } from "~/lib/api";
 import { getRandomResource } from "~/lib/helpers/getRandomResource";
-import { Movie } from "~/lib/types";
+import { Movie, SearchParams } from "~/lib/types";
 
-export default async function MoviesPage() {
+export default async function MoviesPage({ searchParams }: { searchParams: SearchParams }) {
   const [topRated, popular, nowPlaying] = await Promise.all([
     getTopRated("movie"),
     getPopular("movie"),
     getMoviesNowPlaying(),
   ]);
 
-  const randomTopRated = getRandomResource<Movie>(topRated.results);
+  const values =
+    searchParams?.genre && searchParams?.page
+      ? await Promise.all([
+          getByGenre("movie", searchParams?.genre as string, Number(searchParams.page) ?? 1),
+          getGenres("movie"),
+        ])
+      : null;
+
+  const genre =
+    values &&
+    values[1] &&
+    values[1].genres.find(
+      (genre: { id: number; name: string }) => genre.id === Number(searchParams.genre)
+    );
+
+  const randomResource = getRandomResource<Movie>(values ? values[0]?.results : topRated.results);
 
   return (
     <div className="w-full">
-      <Featured item={randomTopRated} />
-      <ItemsList items={popular.results} title="Popular" shortened hrefType="movies" />
-      <ItemsList items={topRated.results} title="Top Rated" shortened hrefType="movies" />
-      <ItemsList items={nowPlaying.results} title="Now Playing" shortened hrefType="movies" />
+      <Featured item={randomResource} />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      {searchParams.genre && searchParams.page && values ? (
+        <ItemsList
+          items={values[0].results}
+          title={genre.name}
+          withPagination
+          currentPage={values[0].page}
+          totalPages={values[0].total_pages}
+        />
+      ) : (
+        <>
+          <ItemsList items={popular.results} title="Popular" shortened hrefType="movies" />
+          <ItemsList items={topRated.results} title="Top Rated" shortened hrefType="movies" />
+          <ItemsList items={nowPlaying.results} title="Now Playing" shortened hrefType="movies" />
+        </>
+      )}
     </div>
   );
 }
