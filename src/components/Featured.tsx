60912428e91072dc702c4cc727505fc1cb6941f9
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-24 09:29:00 +0200

    restore 450 px height for Featured

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -10,7 +10,7 @@ import { Resource } from "~/lib/types";
 
 export function Featured({ item }: { item: Resource }) {
   return (
-    <div className="w-full h-[400px] lg:h-[650px] relative">
+    <div className="w-full h-[450px] lg:h-[650px] relative">
       <BackdropImage item={item} />
 
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/70">
