3b6c76d98cfc7051910f5c0d153e4250babbd1c8
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-24 09:30:48 +0200

    handle add to watchlist functionality, update styles

--- src/components/TvShowDetails.tsx
+++ src/components/TvShowDetails.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { IconStarFilled } from "@tabler/icons-react";
+import { IconHeart, IconHeartFilled, IconStarFilled } from "@tabler/icons-react";
 import Image from "next/image";
+import { useEffect } from "react";
 import { POSTER_IMAGE } from "~/lib/constants";
+import { BREAKPOINTS } from "~/lib/constants/breakpoints";
 import { convertTime } from "~/lib/helpers/convertTime";
 import { getAverage } from "~/lib/helpers/getAverage";
+import { useMediaQuery } from "~/lib/hooks/useMediaQuery";
+import { useWatchlistStore, WatchlistItem } from "~/lib/stores/watchlist-store";
 import { Resource, TvShowDetails } from "~/lib/types";
 import { Carousel } from "./Carousel";
 
@@ -17,6 +21,7 @@ export function TvShowDetails({ item, similar }: { item: TvShowDetails; similar:
       </div>
 
       <div className="pt-8">
+        <h5 className="pb-4 text-3xl font-semibold tracking-tighter">Similar Tv Shows</h5>
         <Carousel items={similar} />
       </div>
     </div>
@@ -37,6 +42,32 @@ function TvShowPosterImage({ src, title }: { src?: string; title: string }) {
 }
 
 function TvShowDetailsInfo({ item }: { item: TvShowDetails }) {
+  const matchesLargeScreen = useMediaQuery(`(min-width: ${BREAKPOINTS.LG}px)`);
+  const watchlist = useWatchlistStore((store) => store.watchlist);
+  const setWatchlist = useWatchlistStore((store) => store.setWatchlist);
+
+  useEffect(() => {
+    useWatchlistStore.persist.rehydrate();
+  }, []);
+
+  const addToWatchList = (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => {
+    e.preventDefault();
+
+    if ((watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id)) {
+      setWatchlist(watchlist.filter((itemInList: Resource) => itemInList.id !== item.id));
+    } else {
+      const newItem = {
+        id: item.id,
+        ...(item.title && { title: item.title }),
+        ...(item.name && { name: item.name }),
+        ...(item.poster_path && { poster_path: item.poster_path }),
+        resourceType: "tv",
+      } as WatchlistItem;
+
+      setWatchlist([...watchlist, newItem]);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-between pt-8 lg:pt-0">
       <div>
@@ -44,6 +75,21 @@ function TvShowDetailsInfo({ item }: { item: TvShowDetails }) {
         <p>{item.tagline}</p>
         <TvShowDetailsInfoList item={item} />
       </div>
+
+      {!matchesLargeScreen ? (
+        <div className="pt-4 lg:pt-0">
+          <button
+            onClick={(e) => addToWatchList(e, item)}
+            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white bg-transparent border rounded-md shadow-sm border-pink-500/70 w-fit focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+          >
+            {(watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id) ? (
+              <IconHeartFilled className="text-pink-500/70" />
+            ) : (
+              <IconHeart className="text-pink-500/70" />
+            )}
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
@@ -86,7 +132,7 @@ function TvShowDetailsInfoList({ item }: { item: TvShowDetails }) {
         <span>{convertTime(getAverage(item.episode_run_time))}</span>
       </li>
       <li className="flex items-center">
-        <span className="w-[120px]">Networks</span>
+        <span className="min-w-[120px] w-fit overflow-hidden">Networks</span>
         <div className="flex flex-wrap space-x-1">
           {item.networks.map((network, index) => (
             <span key={network.id}>
@@ -97,7 +143,7 @@ function TvShowDetailsInfoList({ item }: { item: TvShowDetails }) {
         </div>
       </li>
       <li className="flex items-center">
-        <span className="w-[120px]">Genres</span>
+        <span className="min-w-[120px] w-fit overflow-hidden">Genres</span>
         <div className="flex flex-wrap space-x-1">
           {item.genres.map((genre, index) => (
             <span key={genre.id}>
@@ -108,7 +154,7 @@ function TvShowDetailsInfoList({ item }: { item: TvShowDetails }) {
         </div>
       </li>
       <li className="flex items-center">
-        <span className="w-[120px]">Languages</span>
+        <span className="min-w-[120px] w-fit overflow-hidden">Languages</span>
         <div className="flex flex-wrap space-x-1">
           {item.spoken_languages.map((language, index) => (
             <span key={language.name}>
