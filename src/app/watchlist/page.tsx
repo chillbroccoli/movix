25fcb653acfd3e33546feb045646e3a833884cad
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-24 09:28:26 +0200

    display movies and tv shows from watchlist array

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { useEffect } from "react";
 import { ItemsList } from "~/components/ItemsList";
 import { ViewToggle } from "~/components/ViewToggle";
-import { useLocalStorage } from "~/lib/hooks/useLocalStorage";
+import { useWatchlistStore, WatchlistItem } from "~/lib/stores/watchlist-store";
 
 export default function WatchlistPage() {
-  const [watchlist] = useLocalStorage("watchlist");
+  const watchlist = useWatchlistStore((store) => store.watchlist);
+
+  useEffect(() => {
+    useWatchlistStore.persist.rehydrate();
+  }, []);
+
+  const movies = watchlist.filter((item: WatchlistItem) => item.resourceType === "movie");
+  const tvShows = watchlist.filter((item: WatchlistItem) => item.resourceType === "tv");
 
   return (
     <div>
       <div className="pt-3">
         <ViewToggle />
       </div>
-      <ItemsList title="Your watchlist" items={watchlist} />
+      <ItemsList title="Movies" items={movies} hrefType="movies" />
+      <ItemsList title="Tv Shows" items={tvShows} hrefType="tv" />
     </div>
   );
 }
