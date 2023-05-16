33a9ad846385320ddc34d89f62655eaf4d27f8c0
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-16 19:38:49 +0200

    code refactor

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -11,7 +11,7 @@ import { Resource } from "~/lib/types";
 import { Pagination } from "./Pagination";
 import { Carousel } from "./Carousel";
 import { DisplayMode, useSettingsStore } from "~/lib/stores/settings-store";
-import { useWatchlistStore, WatchlistItem } from "~/lib/stores/watchlist-store";
+import { useWatchlistStore } from "~/lib/stores/watchlist-store";
 import { useEffect } from "react";
 
 export function ItemsList({
@@ -113,50 +113,36 @@ export function ItemsList({
 
 function ListItem({ item, hrefType }: { item: Resource; hrefType: "movies" | "tv" }) {
   const watchlist = useWatchlistStore((store) => store.watchlist);
-  const setWatchlist = useWatchlistStore((store) => store.setWatchlist);
+  const addToWatchlist = useWatchlistStore((store) => store.addToWatchlist);
+  const itemInWatchlist = (watchlist ?? []).find(
+    (itemInList: Resource) => itemInList.id === item.id
+  );
+  const linkHref = hrefType
+    ? `/${hrefType}/${item.id}`
+    : item.media_type === "movie"
+    ? `/movies/${item.id}`
+    : `/tv/${item.id}`;
 
   useEffect(() => {
     useWatchlistStore.persist.rehydrate();
   }, []);
 
-  const addToWatchList = (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => {
+  const handleAddToWatchlistClick = (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => {
     e.preventDefault();
 
-    if ((watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id)) {
-      setWatchlist(watchlist.filter((itemInList: Resource) => itemInList.id !== item.id));
-    } else {
-      const newItem = {
-        id: item.id,
-        ...(item.title && { title: item.title }),
-        ...(item.name && { name: item.name }),
-        ...(item.poster_path && { poster_path: item.poster_path }),
-        resourceType: hrefType === "movies" ? "movie" : "tv",
-      } as WatchlistItem;
-
-      setWatchlist([...watchlist, newItem]);
-    }
+    addToWatchlist(item, hrefType);
   };
 
   return (
-    <Link
-      href={
-        item?.media_type === "movie"
-          ? `/movies/${item.id}`
-          : item?.media_type === "tv"
-          ? `/tv/${item.id}`
-          : `/${hrefType}/${item.id}`
-      }
-      className="group"
-      prefetch={false}
-    >
+    <Link href={linkHref} className="group" prefetch={false}>
       <div className="relative overflow-hidden rounded-md shadow-sm bg-zinc-800 shadow-white/20">
         <div className="relative">
           <ListItemPosterImage item={item} />
           <button
-            onClick={(e) => addToWatchList(e, item)}
+            onClick={(event) => handleAddToWatchlistClick(event, item)}
             className="absolute rounded-bl-xl top-0 p-2 -right-[100%] bg-pink-500/80 group-hover:-right-0 transition-all duration-300 ease-in-out z-10"
           >
-            {(watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id) ? (
+            {itemInWatchlist ? (
               <IconHeartFilled className="text-white" />
             ) : (
               <IconHeart className="text-white" />
@@ -172,11 +158,14 @@ function ListItem({ item, hrefType }: { item: Resource; hrefType: "movies" | "tv
 }
 
 function ListItemPosterImage({ item }: { item: Resource }) {
+  const imageSrc = item.poster_path ? `${POSTER_IMAGE.W342}${item.poster_path}` : imageNotFound;
+  const imageAlt = item?.title ?? item?.name ?? "";
+
   return (
     <div className="relative w-full h-[300px] overflow-hidden">
       <Image
-        src={item.poster_path ? `${POSTER_IMAGE.W342}${item.poster_path}` : imageNotFound}
-        alt={item?.title ?? item?.name ?? ""}
+        src={imageSrc}
+        alt={imageAlt}
         fill
         className="transition-all duration-200 ease-in-out group-hover:scale-110"
       />
@@ -185,6 +174,9 @@ function ListItemPosterImage({ item }: { item: Resource }) {
 }
 
 function ListItemInfo({ item }: { item: Resource }) {
+  const addPadding = item.vote_average && (item?.release_date || item?.first_air_date);
+  const title = item?.title ?? item?.name ?? "";
+
   return (
     <div className="p-2">
       <div className="flex items-center mt-1 space-x-2">
@@ -201,10 +193,10 @@ function ListItemInfo({ item }: { item: Resource }) {
       <p
         className={clsx(
           "pb-1 text-sm font-extralight text-zinc-100 flex items-end h-[50px]",
-          item.vote_average && (item?.release_date || item?.first_air_date) && "pt-3"
+          addPadding && "pt-3"
         )}
       >
-        {item?.title ?? item?.name ?? ""}
+        {title}
       </p>
     </div>
   );
