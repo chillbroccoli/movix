d50375a3d4b2765b995841ae126d976b44d7f9b1
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-16 19:38:56 +0200

    code refactor

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,8 +1,10 @@
+import { HrefType } from "../types";
+
 const BASE_URL = `https://api.themoviedb.org/3`;
 const API_KEY = process.env.API_KEY;
 
-export async function getDetails(type: "movie" | "tv", id: string) {
-  const res = await fetch(`${BASE_URL}/${type}/${id}?api_key=${API_KEY}`);
+export async function getResource(url: string) {
+  const res = await fetch(url);
   const data = await res.json();
 
   if (data.success === false) {
@@ -12,120 +14,44 @@ export async function getDetails(type: "movie" | "tv", id: string) {
   return data;
 }
 
-export async function getTrending(type: "movie" | "tv") {
-  const res = await fetch(`${BASE_URL}/trending/${type}/day?api_key=${API_KEY}`);
-  const data = await res.json();
+export const getDetails = async (type: HrefType, id: string) =>
+  await getResource(`${BASE_URL}/${type}/${id}?api_key=${API_KEY}`);
 
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
+export const getTrending = async (type: HrefType) =>
+  await getResource(`${BASE_URL}/trending/${type}/day?api_key=${API_KEY}`);
 
-  return data;
-}
+export const getTopRated = async (type: HrefType) =>
+  await getResource(`${BASE_URL}/${type}/top_rated?api_key=${API_KEY}`);
 
-export async function getTopRated(type: "movie" | "tv") {
-  const res = await fetch(`${BASE_URL}/${type}/top_rated?api_key=${API_KEY}`);
-  const data = await res.json();
+export const getPopular = async (type: HrefType) =>
+  await getResource(`${BASE_URL}/${type}/popular?api_key=${API_KEY}`);
 
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
+export const getMoviesNowPlaying = async () =>
+  await getResource(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`);
 
-  return data;
-}
+export const getLatest = async (type: HrefType) =>
+  await getResource(`${BASE_URL}/${type}/latest?api_key=${API_KEY}`);
 
-export async function getPopular(type: "movie" | "tv") {
-  const res = await fetch(`${BASE_URL}/${type}/popular?api_key=${API_KEY}`);
-  const data = await res.json();
+export const getAiringToday = async () =>
+  await getResource(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`);
 
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
-
-export async function getMoviesNowPlaying() {
-  const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`);
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
-
-export async function getLatest(type: "movie" | "tv") {
-  const res = await fetch(`${BASE_URL}/${type}/latest?api_key=${API_KEY}`);
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
-
-export async function getAiringToday() {
-  const res = await fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`);
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
-
-export async function getSimilar(type: "movie" | "tv", id: string) {
-  const res = await fetch(`${BASE_URL}/${type}/${id}/similar?api_key=${API_KEY}`);
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
+export const getSimilar = async (type: HrefType, id: string) =>
+  await getResource(`${BASE_URL}/${type}/${id}/similar?api_key=${API_KEY}`);
 
 export async function multiSearch(query?: string) {
   if (!query) {
     return null;
   }
 
-  const res = await fetch(`${BASE_URL}/search/multi?api_key=${API_KEY}&query=${query}`);
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
+  return await getResource(`${BASE_URL}/search/multi?api_key=${API_KEY}&query=${query}`);
 }
 
-export async function getGenres(type: "movie" | "tv") {
-  const res = await fetch(
+export const getGenres = async (type: HrefType) =>
+  await getResource(
     `${BASE_URL}/genre/${type}/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
   );
-  const data = await res.json();
 
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
-
-export async function getByGenre(type: "movie" | "tv", genreId: string, page = 1) {
-  const res = await fetch(
+export const getByGenre = async (type: HrefType, genreId: string, page = 1) =>
+  await getResource(
     `${BASE_URL}/discover/${type}?api_key=${API_KEY}&with_genres=${genreId}&sort_by=popularity.desc&page=${page}`
   );
-  const data = await res.json();
-
-  if (data.success === false) {
-    throw new Error(data?.status_message ?? "Something went wrong");
-  }
-
-  return data;
-}
