d7a8f73dff45dc5b862f81cf908c11555df6d123
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-16 19:38:44 +0200

    code refactor

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,28 +18,16 @@ export function Carousel({
   hrefType?: "movies" | "tv";
 }) {
   const watchlist = useWatchlistStore((store) => store.watchlist);
-  const setWatchlist = useWatchlistStore((store) => store.setWatchlist);
+  const addToWatchlist = useWatchlistStore((store) => store.addToWatchlist);
 
   useEffect(() => {
     useWatchlistStore.persist.rehydrate();
   }, []);
 
-  const addToWatchList = (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => {
+  const handleAddToWatchlistClick = (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => {
     e.preventDefault();
 
-    if ((watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id)) {
-      setWatchlist(watchlist.filter((itemInList: Resource) => itemInList.id !== item.id));
-    } else {
-      const newItem = {
-        id: item.id,
-        ...(item.title && { title: item.title }),
-        ...(item.name && { name: item.name }),
-        ...(item.poster_path && { poster_path: item.poster_path }),
-        resourceType: hrefType === "movies" ? "movie" : "tv",
-      } as WatchlistItem;
-
-      setWatchlist([...watchlist, newItem]);
-    }
+    addToWatchlist(item, hrefType);
   };
 
   return (
@@ -51,7 +39,7 @@ export function Carousel({
             item={item}
             hrefType={hrefType}
             watchlist={watchlist}
-            addToWatchList={addToWatchList}
+            handleAddToWatchlistClick={handleAddToWatchlistClick}
           />
         ))}
       </div>
@@ -63,27 +51,29 @@ function CarouselItem({
   item,
   hrefType,
   watchlist,
-  addToWatchList,
+  handleAddToWatchlistClick,
 }: {
   item: Resource;
   hrefType: "movies" | "tv";
   watchlist: WatchlistItem[];
-  addToWatchList: (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => void;
+  handleAddToWatchlistClick: (e: React.MouseEvent<HTMLButtonElement>, item: Resource) => void;
 }) {
+  const itemInWatchlist = (watchlist ?? []).find(
+    (itemInList: Resource) => itemInList.id === item.id
+  );
+  const imageSrc = item.poster_path ? `${POSTER_IMAGE.W342}${item.poster_path}` : imageNotFound;
+  const imageAlt = item?.title ?? item?.name ?? "";
+
   return (
     <Link href={`/${hrefType}/${item.id}`} className="relative rounded-md group">
       <div className="snap-start w-[225px] h-[350px] flex-shrink-0 shadow-md shadow-white/20 rounded-md overflow-hidden">
         <div className="relative w-full h-full">
-          <Image
-            src={item.poster_path ? `${POSTER_IMAGE.W342}${item.poster_path}` : imageNotFound}
-            alt={item?.title ?? item?.name ?? ""}
-            fill
-          />
+          <Image src={imageSrc} alt={imageAlt} fill />
           <button
-            onClick={(e) => addToWatchList(e, item)}
+            onClick={(event) => handleAddToWatchlistClick(event, item)}
             className="absolute rounded-bl-xl top-0 p-2 -right-[100%] bg-pink-500/80 group-hover:-right-0 transition-all duration-300 ease-in-out z-10"
           >
-            {(watchlist ?? []).find((itemInList: Resource) => itemInList.id === item.id) ? (
+            {itemInWatchlist ? (
               <IconHeartFilled className="text-white" />
             ) : (
               <IconHeart className="text-white" />
