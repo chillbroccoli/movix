e0885210b336b60b8ddc45fcc334f46517d99d32
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 14:50:20 +0200

    extract rsc jsx to view component

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -1,9 +1,5 @@
-import { Featured } from "~/components/Featured";
-import { ItemsList } from "~/components/ItemsList";
-import { ViewToggle } from "~/components/ViewToggle";
 import { getPopular, getTopRated, getTrending } from "~/lib/api";
-import { getRandomResource } from "~/lib/helpers/getRandomResource";
-import { TvShow } from "~/lib/types";
+import { TvShowsView } from "~/views/TvShows.view";
 
 export default async function TvShowsPage() {
   const [topRated, popular, trending] = await Promise.all([
@@ -12,27 +8,5 @@ export default async function TvShowsPage() {
     getTrending("tv"),
   ]);
 
-  const randomResource = getRandomResource<TvShow>(topRated.results);
-
-  const popularSorted = popular.results.sort(
-    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
-  );
-  const topRatedSorted = topRated.results.sort(
-    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
-  );
-  const trendingSorted = trending.results.sort(
-    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
-  );
-
-  return (
-    <div className="w-full">
-      <Featured item={randomResource} />
-      <div className="pt-3">
-        <ViewToggle />
-      </div>
-      <ItemsList items={topRatedSorted} title="Top Rated" shortened hrefType="tv" />
-      <ItemsList items={popularSorted} title="Popular" shortened hrefType="tv" />
-      <ItemsList items={trendingSorted} title="Trending" shortened hrefType="tv" />
-    </div>
-  );
+  return <TvShowsView topRated={topRated} popular={popular} trending={trending} />;
 }
---
+++ src/views/TvShows.view.tsx
@@ -0,0 +1,37 @@
+import { Featured } from "~/components/Featured";
+import { ItemsList } from "~/components/ItemsList";
+import { ViewToggle } from "~/components/ViewToggle";
+import { getRandomResource } from "~/lib/helpers/getRandomResource";
+import { ListResponse, TvShow } from "~/lib/types";
+
+type TvShowsViewProps = {
+  popular: ListResponse<TvShow>;
+  topRated: ListResponse<TvShow>;
+  trending: ListResponse<TvShow>;
+};
+
+export function TvShowsView({ popular, topRated, trending }: TvShowsViewProps) {
+  const randomResource = getRandomResource<TvShow>(topRated.results);
+
+  const popularSorted = popular.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+  const topRatedSorted = topRated.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+  const trendingSorted = trending.results.sort(
+    (a: TvShow, b: TvShow) => b.vote_average - a.vote_average
+  );
+
+  return (
+    <div className="w-full">
+      <Featured item={randomResource} />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      <ItemsList items={topRatedSorted} title="Top Rated" shortened hrefType="tv" />
+      <ItemsList items={popularSorted} title="Popular" shortened hrefType="tv" />
+      <ItemsList items={trendingSorted} title="Trending" shortened hrefType="tv" />
+    </div>
+  );
+}
