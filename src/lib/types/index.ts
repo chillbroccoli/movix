6617a923c16e0403e173a9c2c98e92e59a7704ed
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 12:28:08 +0200

    add new type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,12 @@
 export type SearchParams = { [key: string]: string | string[] | undefined };
 
+export interface ListResponse<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
+
 export type Trending = {
   id: number;
   adult: boolean;
