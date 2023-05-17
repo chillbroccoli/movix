de0e34fb6d95c7f540b6c0675cd3327192eae886
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 14:41:33 +0200

    extract rsc jsx to view component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import { Featured } from "~/components/Featured";
-import { ItemsList } from "~/components/ItemsList";
-import { ViewToggle } from "~/components/ViewToggle";
 import { getTrending } from "~/lib/api";
-import { getRandomResource } from "~/lib/helpers/getRandomResource";
-import { Movie, TvShow } from "~/lib/types";
+import { HomeView } from "~/views/Home.view";
 
 export default async function HomePage() {
   const moviesData = await getTrending("movie");
   const tvData = await getTrending("tv");
 
-  const randomTrending = getRandomResource<Movie | TvShow>([
-    ...moviesData.results,
-    ...tvData.results,
-  ]);
-
-  return (
-    <div className="w-full h-full">
-      <Featured item={randomTrending} />
-      <div className="pt-3">
-        <ViewToggle />
-      </div>
-      <ItemsList items={moviesData.results} title="Trending Movies" hrefType="movies" />
-      <ItemsList items={tvData.results} title="Trending Tv Shows" hrefType="tv" />
-    </div>
-  );
+  return <HomeView movies={moviesData} tvShows={tvData} />;
 }
---
+++ src/views/Home.view.tsx
@@ -0,0 +1,25 @@
+import { Featured } from "~/components/Featured";
+import { ItemsList } from "~/components/ItemsList";
+import { ViewToggle } from "~/components/ViewToggle";
+import { getRandomResource } from "~/lib/helpers/getRandomResource";
+import { ListResponse, Movie, TvShow } from "~/lib/types";
+
+type HomeViewProps = {
+  movies: ListResponse<Movie>;
+  tvShows: ListResponse<TvShow>;
+};
+
+export function HomeView({ movies, tvShows }: HomeViewProps) {
+  const randomTrending = getRandomResource<Movie | TvShow>([...movies.results, ...tvShows.results]);
+
+  return (
+    <div className="w-full h-full">
+      <Featured item={randomTrending} />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      <ItemsList items={movies.results} title="Trending Movies" hrefType="movies" />
+      <ItemsList items={tvShows.results} title="Trending Tv Shows" hrefType="tv" />
+    </div>
+  );
+}
