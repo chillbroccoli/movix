4b4a920e19f61b26784c7cafb1aa7d60e9692b34
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 14:44:05 +0200

    extract rsc jsx to view component

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,28 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-
-import { ItemsList } from "~/components/ItemsList";
-import { ViewToggle } from "~/components/ViewToggle";
-import { useWatchlistStore, WatchlistItem } from "~/lib/stores/watchlist-store";
+import { WatchlistView } from "~/views/Watchlist.view";
 
 export default function WatchlistPage() {
-  const watchlist = useWatchlistStore((store) => store.watchlist);
-
-  useEffect(() => {
-    useWatchlistStore.persist.rehydrate();
-  }, []);
-
-  const movies = watchlist.filter((item: WatchlistItem) => item.resourceType === "movie");
-  const tvShows = watchlist.filter((item: WatchlistItem) => item.resourceType === "tv");
-
-  return (
-    <div>
-      <div className="pt-3">
-        <ViewToggle />
-      </div>
-      <ItemsList title="Movies" items={movies} hrefType="movies" />
-      <ItemsList title="Tv Shows" items={tvShows} hrefType="tv" />
-    </div>
-  );
+  return <WatchlistView />;
 }
---
+++ src/views/Watchlist.view.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { ItemsList } from "~/components/ItemsList";
+import { ViewToggle } from "~/components/ViewToggle";
+import { useWatchlistStore, WatchlistItem } from "~/lib/stores/watchlist-store";
+
+export function WatchlistView() {
+  const watchlist = useWatchlistStore((store) => store.watchlist);
+
+  useEffect(() => {
+    useWatchlistStore.persist.rehydrate();
+  }, []);
+
+  const movies = watchlist.filter((item: WatchlistItem) => item.resourceType === "movie");
+  const tvShows = watchlist.filter((item: WatchlistItem) => item.resourceType === "tv");
+
+  return (
+    <div>
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      <ItemsList title="Movies" items={movies} hrefType="movies" />
+      <ItemsList title="Tv Shows" items={tvShows} hrefType="tv" />
+    </div>
+  );
+}
