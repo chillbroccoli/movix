3bf500ac83693d51c02acd6d13ccf2d8426067ff
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 12:27:11 +0200

    add tv by genre page

---
+++ src/app/tv/by-genre/page.tsx
@@ -0,0 +1,19 @@
+import { getByGenre, getGenres } from "~/lib/api";
+import { getRandomResource } from "~/lib/helpers/getRandomResource";
+import { Genre, SearchParams, TvShow } from "~/lib/types";
+import { TvByGenreView } from "~/views/TvByGenre.view";
+
+export default async function TvByGenrePage({ searchParams }: { searchParams: SearchParams }) {
+  console.log("searchParams?.id", searchParams?.id);
+
+  const [tv, genres] = await Promise.all([
+    getByGenre("tv", searchParams?.id as string, Number(searchParams.page) ?? 1),
+    getGenres("tv"),
+  ]);
+
+  const genre = genres.genres.find((genre: Genre) => genre.id === Number(searchParams?.id));
+
+  const randomResource = getRandomResource<TvShow>(tv.results);
+
+  return <TvByGenreView data={tv} genre={genre} randomResource={randomResource} />;
+}
