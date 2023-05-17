31ae2efab71f5570673b47d104cab23de7ed6f60
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-17 12:28:18 +0200

    add MoviesByGenre view

---
+++ src/views/MoviesByGenre.view.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Featured } from "~/components/Featured";
+import { ItemsList } from "~/components/ItemsList";
+import { ViewToggle } from "~/components/ViewToggle";
+import type { Genre, ListResponse, Movie } from "~/lib/types";
+
+type MoviesByGenreViewProps = {
+  data: ListResponse<Movie>;
+  randomResource: Movie;
+  genre: Genre;
+};
+
+export function MoviesByGenreView({ data, randomResource, genre }: MoviesByGenreViewProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const generatePath = (page: "next" | "prev") => {
+    const params = new URLSearchParams();
+    const genre = searchParams.get("id") ?? "";
+    const currentPage = Number(searchParams.get("page")) ?? 1;
+    params.set("id", genre);
+    params.set("page", String((currentPage ?? 1) + (page === "next" ? 1 : -1)));
+    return decodeURIComponent(`${pathname}?${params.toString()}`);
+  };
+
+  const goToNextPage = () => {
+    router.push(generatePath("next"));
+  };
+
+  const goToPreviousPage = () => {
+    router.push(generatePath("prev"));
+  };
+
+  return (
+    <div className="w-full">
+      <Featured item={randomResource} />
+      <div className="pt-3">
+        <ViewToggle />
+      </div>
+      <ItemsList
+        items={data.results}
+        title={genre.name}
+        withPagination
+        goToNextPage={goToNextPage}
+        goToPreviousPage={goToPreviousPage}
+        currentPage={data.page}
+        totalPages={data.total_pages}
+      />
+    </div>
+  );
+}
